e9b83b3e1392f7d132d29bc1b6d258e0bd7256b6
tosumaso <wakuwaku5712@gmail.com>
2020-11-03 21:33:32 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,56 @@
+class PrototypesController < ApplicationController
+before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+before_action :move_to_edit, only: [:edit]
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    prototype =Prototype.new(prototype_params)
+    if prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype= Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_edit
+    unless Prototype.find(params[:id]).user.id.to_i == current_user.id
+      redirect_to root_path
+    end
+  end
+end
